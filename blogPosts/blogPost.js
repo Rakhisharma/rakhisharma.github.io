e120994612c969501175d4ff231af3be9405367f
TemporaryAdmin <rakhi.sharma@deliveryhero.com>
2021-03-06 16:25:27 +0100

    update blogPost

--- blogPosts/blogPost.js
+++ blogPosts/blogPost.js
@@ -1,8 +1,58 @@
 let posts = [
     {
+		"title": "Testing in Firefox- Mochitest!",
+		"link": "https://rakhish.wordpress.com/2016/07/12/major-improvement-in-firefox/",
+		"publishDate": "12 July 2020",
+	},
+    {
+		"title": "Testing in Firefox- Mochitest!",
+		"link": "https://rakhish.wordpress.com/2016/07/12/major-improvement-in-firefox/",
+		"publishDate": "12 July 2020",
+	},
+    {
+		"title": "Testing in Firefox- Mochitest!",
+		"link": "https://rakhish.wordpress.com/2016/07/12/major-improvement-in-firefox/",
+		"publishDate": "12 July 2020",
+	},
+    {
+		"title": "Testing in Firefox- Mochitest!",
+		"link": "https://rakhish.wordpress.com/2016/07/12/major-improvement-in-firefox/",
+		"publishDate": "12 July 2020",
+	},
+    {
+		"title": "Testing in Firefox- Mochitest!",
+		"link": "https://rakhish.wordpress.com/2016/07/28/testing-in-firefox-mochitest/",
+		"publishDate": "28 July 2020",
+	},
+    {
+		"title": "Major improvement in Firefox",
+		"link": "https://rakhish.wordpress.com/2016/07/12/major-improvement-in-firefox/",
+		"publishDate": "12 July 2020",
+	},
+    {
+		"title": "Open source, Mozilla & Outreachy",
+		"link": "https://rakhish.wordpress.com/2016/07/11/374/",
+		"publishDate": "11 July 2016",
+	},
+    {
+		"title": "Hg push-to-try",
+		"link": "https://rakhish.wordpress.com/2016/06/27/hg-push-to-try/",
+		"publishDate": "27 June 2020",
+	},
+    {
+		"title": "Bookmark in toolbar.",
+		"link": "https://rakhish.wordpress.com/2016/06/25/bookmark-in-toolbar/",
+		"publishDate": "25 June 2020",
+	},
+    {
+		"title": "Firefox for Desktop.",
+		"link": "https://rakhish.wordpress.com/2016/05/30/firefix-for-desktop/",
+		"publishDate": "30 May 2016",
+	},
+    {
 		"title": "Outreachy- The Beginning!",
 		"link": "https://rakhish.wordpress.com/2016/05/23/outreachy-the-beginning/",
-		"publishDate": "05 May 2020",
+		"publishDate": "23 May 2016",
 	},
     {
 		"title": "Shotgun-game [Learn IT, Girl]",
