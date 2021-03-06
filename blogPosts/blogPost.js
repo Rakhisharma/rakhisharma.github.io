0cc293e09135025b57b559e27ff3a16eb62b787b
TemporaryAdmin <rakhi.sharma@deliveryhero.com>
2021-03-06 16:02:35 +0100

    update blogPost

--- blogPosts/blogPost.js
+++ blogPosts/blogPost.js
@@ -1,8 +1,23 @@
 let posts = [
+    {
+		"title": "Outreachy- The Beginning!",
+		"link": "https://rakhish.wordpress.com/2016/05/23/outreachy-the-beginning/",
+		"publishDate": "05 May 2020",
+	},
+    {
+		"title": "Shotgun-game [Learn IT, Girl]",
+		"link": "https://rakhish.wordpress.com/2016/05/09/shotgun-maths-game/",
+		"publishDate": "09 May 2016",
+	},
+    {
+		"title": "Import image using openGL in C",
+		"link": "https://rakhish.wordpress.com/2016/03/29/import-image-using-opengl-in-c/",
+		"publishDate": "29 Mar 2016",
+	},
 	{
-		"title": "Test1",
-		"link": "https://rakhisharma.github.io/writing.html",
-		"publishDate": "3 Mar 2021",
+		"title": "Learn IT, Girl!",
+		"link": "https://rakhish.wordpress.com/2016/03/12/learn-it-girl/",
+		"publishDate": "12 Mar 2016",
 	}
 ],
 posts_html = '';
